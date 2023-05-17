29a1737cc3c02ba0d201260adea3c86be56e7da4
mary_waiyego <waiyegomary55@gmail.com>
2023-05-17 10:06:11 +0300

    methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer()
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name )
+    puts "Hello, #{name}!"
+end
+ 
+def  greet_with_default (name= "programmer")
+    puts "Hello, #{name}!"
+end 
+
+def add (num1, num2)
+    num1+num2 
+end
+add(2,5)
+
+def halve (number)
+    if (number.class != Integer)
+     return nil
+    end
+    number/2
+    
+end 
+halve(6)
+
+
